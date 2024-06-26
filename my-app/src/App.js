2dbe8ac4e9ffcc7a520fb964a524d8b8c3950e8f
Simran Singh <m.simx2909@gmail.com>
2024-06-26 17:53:11 +0530

    Added User-Image

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,23 @@
-function MyButton() {
-  return (
-      <button>
-        I'm a button
-      </button>
-  );
-}
+const user={
+    name:"Shih-Tzu Singh Sandral",
+    imageUrl:"https://puppiezo.com/wp-content/uploads/2023/06/IMG_1820.jpeg",
+    imageSize:90,
+};
 
-export default function MyApp() {
-  return (
-      <div>
-        <h1>Welcome to my app</h1>
-        <MyButton />
-      </div>
-  );
+export default function Profile(){
+    return (
+      <>
+          <strong>{user.name}</strong>
+          <img
+              className="profile-image"
+              src={user.imageUrl}
+              alt={"Photo of "+user.name}
+              style={{
+                  width:user.imageSize+"px",
+                  height:user.imageSize+"px",
+                  borderRadius:user.imageSize/2+"px",
+              }}
+          />
+      </>
+    );
 }
