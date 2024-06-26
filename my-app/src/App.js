6177988dec9bdc16f5f6d6ce6edaaeebf3f6b153
Simran Singh <m.simx2909@gmail.com>
2024-06-26 19:58:30 +0530

    Click Counter Added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,22 +1,26 @@
-const products = [
-    { title: 'Cabbage', isFruit: false, id: 1 },
-    { title: 'Garlic', isFruit: false, id: 2 },
-    { title: 'Apple', isFruit: true, id: 3 },
-];
+import { useState } from 'react';
 
-export default function ShoppingList() {
-    const listItems = products.map(product =>
-        <li
-            key={product.id}
-            style={{
-                color: product.isFruit ? 'magenta' : 'darkgreen'
-            }}
-        >
-            {product.title}
-        </li>
+export default function MyApp() {
+    return (
+        <div>
+            <h1>Counters that update separately</h1>
+            <MyButton />
+            <br></br>
+            <MyButton />
+        </div>
     );
+}
+
+function MyButton() {
+    const [count, setCount] = useState(0);
+
+    function handleClick() {
+        setCount(count + 1);
+    }
 
     return (
-        <ul>{listItems}</ul>
+        <button onClick={handleClick}>
+            Clicked {count} number of times
+        </button>
     );
 }
